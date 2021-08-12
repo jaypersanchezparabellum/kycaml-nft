45077172ff4e0facb7da47e63349dfaab11f153c
jaypersanchez <jaypersanchez@gmail.com>
2021-08-12 09:11:39 +0800

    saving field state

--- src/components/SelectiveDisclosureScreen.js
+++ src/components/SelectiveDisclosureScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, StyleSheet } from 'react';
 import { 
     Container, 
     Row, 
@@ -13,11 +13,60 @@ import VerifyButton from "@passbase/button/react";
 
 function SelectiveDisclosureScreen() {
 
+    
+    
+    // Declare a new state variable, which we'll call "count"
+    const [firstname, setfirstname] = useState();
+    const [middlename, setmiddlename] = useState();
+    const [lastname, setlastname] = useState();
+    const [dob, setdob] = useState();
+    const [email, setemail] = useState();
+    const [mobile, setmobile] = useState();
+    const [inclusion1, setinclusion1] = useState();
+    const [address1, setaddress1] = useState();
+    const [address2, setaddress2] = useState();
+    const [city, setcity] = useState();
+    const [stateprovince, setstateprovince] = useState();
+    const [country, setcountry] = useState();
+    const [zippostal, setzippostal] = useState();
+    const [inclusion2, setinclusion2] =  useState();
+    const [countrydob, setcountrydob] = useState();
+    const [countrycitizenship, setcountrycitizenship] = useState();
+    const [height, setheight] = useState();
+    const [weight, setweight] = useState();
+    const [eyecolor, seteyecolor] = useState();
+    const [haircolor, sethaircolor] = useState();
+
+    let state = {};
+
     const referenceUserWithKey = (key) => {
         console.log(key)
         // Make request to your backend/db and save the key to the user's profile
     }
 
+    function saveDataSet() {
+        alert(`${firstname}::${middlename}::${lastname}::${lastname}::${dob}::${email}::${mobile}::${address1}::${address2}::${city}::${stateprovince}::${country}::${zippostal}::${countrydob}::${countrycitizenship}::${height}::${weight}::${eyecolor}`);
+        /*state = {"firstname":firstname};
+        state = {"middlename":middlename};
+        state = {"lastname":lastname}
+        state = {"dob":dob}
+        state = {"email":email}
+        state = {"mobile":mobile}
+        state = {"address1":address1}
+        state = {"address2":address2}
+        state = {"city":city}
+        state = {"stateprovince":stateprovince}
+        state = {"country":country}
+        state = {"zippostal":zippostal}
+        state = {"countrydob":countrydob}
+        state = {"countrycitizenship":countrycitizenship}
+        state = {"height":height}
+        state = {"weight":weight}
+        state = {"eyecolor":eyecolor}
+        state = {"haircolor":haircolor}
+        alert(`${JSON.stringify(state)}`)*/
+    }
+    
     return (
         <Container>
             <Row>
@@ -27,6 +76,7 @@ function SelectiveDisclosureScreen() {
                         placeholder="First Name"
                         aria-label="firstname"
                         aria-describedby="basic-addon1"
+                        onChange={ (event) => { setfirstname(event.target.value) }}
                         />
                     </InputGroup>
                 </Col>
@@ -36,6 +86,7 @@ function SelectiveDisclosureScreen() {
                         placeholder="Middle Name"
                         aria-label="middlename"
                         aria-describedby="basic-addon1"
+                        onChange={ (event) => { setmiddlename(event.target.value) }}
                         />
                     </InputGroup>
                 </Col>
@@ -45,6 +96,7 @@ function SelectiveDisclosureScreen() {
                         placeholder="Last Name"
                         aria-label="lastname"
                         aria-describedby="basic-addon1"
+                        onChange={ (event) => { setlastname(event.target.value) }}
                         />
                     </InputGroup>
                 </Col>
@@ -59,6 +111,7 @@ function SelectiveDisclosureScreen() {
                         placeholder="Email"
                         aria-label="email"
                         aria-describedby="basic-addon2"
+                        onChange={ (event) => { setemail(event.target.value) }}
                         />
                         <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
                     </InputGroup>
@@ -69,6 +122,7 @@ function SelectiveDisclosureScreen() {
                         placeholder="Mobile Number"
                         aria-label="mobile"
                         aria-describedby="basic-addon2"
+                        onChange={ (event) => { setmobile(event.target.value) }}
                         />
                         <InputGroup.Text id="basic-addon2">+1555-555-5555</InputGroup.Text>
                     </InputGroup>
@@ -88,6 +142,7 @@ function SelectiveDisclosureScreen() {
                             placeholder="Address 1"
                             aria-label="address1"
                             aria-describedby="basic-addon2"
+                            onChange={ (event) => { setaddress1(event.target.value) }}
                             />
                         </InputGroup>
                 </Col>
@@ -97,6 +152,7 @@ function SelectiveDisclosureScreen() {
                             placeholder="Address 2"
                             aria-label="address2"
                             aria-describedby="basic-addon2"
+                            onChange={ (event) => { setaddress2(event.target.value) }}
                             />
                         </InputGroup>
                 </Col>
@@ -106,6 +162,7 @@ function SelectiveDisclosureScreen() {
                             placeholder="city"
                             aria-label="city"
                             aria-describedby="basic-addon2"
+                            onChange={ (event) => { setcity(event.target.value) }}
                             />
                         </InputGroup>
                 </Col>
@@ -115,6 +172,7 @@ function SelectiveDisclosureScreen() {
                             placeholder="State/Province"
                             aria-label="stateprovince"
                             aria-describedby="basic-addon2"
+                            onChange={ (event) => { setstateprovince(event.target.value) }}
                             />
                         </InputGroup>
                 </Col>
@@ -142,6 +200,7 @@ function SelectiveDisclosureScreen() {
                             placeholder="Zipcode/Postal Code"
                             aria-label="zipcodepostalcode"
                             aria-describedby="basic-addon2"
+                            onChange={ (event) => { setzippostal(event.target.value) }}
                             />
                         </InputGroup> 
                 </Col>
@@ -194,6 +253,7 @@ function SelectiveDisclosureScreen() {
                             placeholder="Height"
                             aria-label="height"
                             aria-describedby="basic-addon2"
+                            onChange={ (event) => { setheight(event.target.value) }}
                             />
                         </InputGroup> 
                 </Col>
@@ -203,6 +263,7 @@ function SelectiveDisclosureScreen() {
                             placeholder="Weight"
                             aria-label="weight"
                             aria-describedby="basic-addon2"
+                            onChange={ (event) => { setweight(event.target.value) }}
                             />
                         </InputGroup> 
                 </Col>
@@ -238,6 +299,9 @@ function SelectiveDisclosureScreen() {
             </Row>
             <Row>
                 <Col>
+                    <Button variant="primary" color="accent" onClick={saveDataSet} >Save Data Set</Button>
+                </Col>
+                <Col>
                     <VerifyButton
                         apiKey={"TBFIEVmCNIcZ7605NBsoe9kVbM21Sdss6fXlztkkltvZIse6WA6u3NEUfXJXU3CG"}
                         onSubmitted={(identityAccessKey) => {
@@ -247,7 +311,7 @@ function SelectiveDisclosureScreen() {
                         onError={(errorCode) => {}}
                         onStart={() => {}}
                         prefillAttributes={{
-                            email: "hans.meier@corp.de"
+                            email: {firstname}
                         }}
                     />
                 </Col>
@@ -257,6 +321,8 @@ function SelectiveDisclosureScreen() {
             
         
     );
+
   }
+  
   
   export default SelectiveDisclosureScreen;
