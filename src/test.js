4b25a89821bea6728cb9a50977bf9fe4054032ca
jaypersanchez <jaypersanchez@gmail.com>
2021-08-26 06:33:43 +0800

    saving changes

--- src/test.js
+++ src/test.js
@@ -1,8 +1,9 @@
 const crypto = require("crypto");
 const fs = require("fs");
+const metadata = {"parabellum_customer_id": "UID of Nenad", "email": "nenad@email.com", "country": "rs", "note":"I am testing adding metadata to ID verification."};
 
-console.log(`Test Script`)
-const metadata = {foo: "bar"};
+
+//const pkey = crypto.createPrivateKey({format: 'pem', key: pk});
 const pkey = crypto.createPrivateKey({format: 'pem', key: fs.readFileSync("./components/passbase-test-private-key.pem")});
 const encrypted_metadata = crypto.privateEncrypt(pkey, Buffer.from(JSON.stringify(metadata))).toString('base64');
-console.log(`${encrypted_metadata}`)
+console.log(encrypted_metadata);
